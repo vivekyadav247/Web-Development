72b0997cd6705eb1450af635acb93db96b4aaf8d
Vivek Yadav <vivekyad240706@gmail.com>
2025-05-10 23:16:00 +0530

    Array Method PQ completed

--- JavaScript/Practice Questions/Array_Method_practiceQues.js
+++ JavaScript/Practice Questions/Array_Method_practiceQues.js
@@ -25,3 +25,14 @@ console.log(res3)
 
 
 // Solution of Question 4
+const doubleAndReturnArgs = (arr,...args)  => [
+    ...arr,...args.map((variables) => variables*2),
+]
+let res4 = doubleAndReturnArgs([1,2,3],4,5)
+console.log(res4)
+
+
+// Solution of Question 5
+const mergeObjects = (obj1,obj2) => ({...obj1,...obj2})
+let obj = mergeObjects({a:1,b:2},{c:3,d:4})
+console.log(obj)
